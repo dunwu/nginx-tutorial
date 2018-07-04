8b7d89b0b9316d515aad96fc9dc87609f5e7503d
Zhang Peng <forbreak@163.com>
2018-07-04 22:54:41 +0800

    :bookmark: bat script for nginx

--- demos/nginx-1.14.0/nginx-start.bat
+++ demos/nginx-1.14.0/nginx-start.bat
@@ -1,3 +1,17 @@
 @echo off
-nginx.exe -c conf/nginx.conf
-pause
+
+rem 1. 如果目录不存在 logs 目录，则创建
+if not exist %~dp0logs (
+  echo %~dp0logs is not exists, create it.
+  md %~dp0logs
+)
+
+rem 2. 如果目录不存在 temp 目录，则创建
+if not exist %~dp0temp (
+  echo %~dp0temp is not exists, create it.
+  md %~dp0temp
+)
+
+rem 3. 指定 conf/nginx.conf ，启动 nginx
+nginx.exe -t -c conf/nginx.conf
+start nginx
