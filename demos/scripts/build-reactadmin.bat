02cc3daf2944e199484a6a5e28f5e779a2d2a167
Zhang Peng <forbreak@163.com>
2018-10-12 19:10:30 +0800

    :bookmark: demo06

---
+++ demos/scripts/build-reactadmin.bat
@@ -0,0 +1,22 @@
+@echo off
+rem -------------------------------------------------
+rem The script is use to package reactadmin
+rem Env: Nodejs 8.x
+rem -------------------------------------------------
+
+pushd %~dp0..\reactadmin
+
+echo ">>>> 1. Delete node_modules"
+rd /s /q node_modules
+
+echo ">>>> 2. npm install"
+rem 安装本地依赖
+call cnpm install
+
+echo ">>>> 3. npm run prod"
+rem 构建生产环境，构建的静态资源文件在 dist 目录
+call npm run prod
+
+popd
+
+pause
